38368fab35f017a71d2f5d455e907f4fec0f3c02
arr-dev <arr-dev@users.noreply.github.com>
2015-10-13 20:46:06 +0200

    Add pretty output format
    Sort options in result

--- output.go
+++ output.go
@@ -22,9 +22,11 @@ func (o *Output) Format() string {
 	}
 
 	switch o.format {
-	case "plain":
-		return fmt.Sprintf("%v", o.result)
-	case "json":
+	case "plain", "p":
+		return plainFormat(o.result)
+	case "pretty":
+		return prettyFormat(o.result)
+	case "json", "j":
 		ret, err := json.Marshal(o.result)
 		if err == nil {
 			return string(ret)
@@ -32,4 +34,31 @@ func (o *Output) Format() string {
 	}
 
 	return ""
+}
+
+func plainFormat(r Result) string {
+	ret := ""
+	ret += fmt.Sprintf("Host %s\n", r.Host)
+	for _, k := range r.OptionKeys {
+		ret += fmt.Sprintf("    %s %v\n", k, r.Options[k])
+	}
+	return ret
+}
+
+func prettyFormat(r Result) string {
+	maxLength := 0
+	for k, _ := range r.Options {
+		if l := len(k); l > maxLength {
+			maxLength = l
+		}
+	}
+	headerTemplate := fmt.Sprintf("%%-%ds %%s\n", maxLength+4)
+	template := fmt.Sprintf("    %%-%ds %%s\n", maxLength)
+
+	ret := fmt.Sprintf(headerTemplate, "Host", r.Host)
+
+	for _, k := range r.OptionKeys {
+		ret += fmt.Sprintf(template, k, r.Options[k])
+	}
+	return ret
 }
